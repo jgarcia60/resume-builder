a2f8d9a577bb8c6d1d5e4eda213ff75d0cc43d6a
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-12-16 18:44:04 -0500

    changed models to one single resume table and completed form edit handlebars

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
         const $clone = $(".education").first().clone();
         let index = parseInt($(".education").attr("data-index")) + educationCounter;
         index = index.toString();
+        let letter = String.fromCharCode(65 + educationCounter);
+        //update school id
+        $clone.find('.school').attr('id', 'school'+ letter);
+        //update degree id
+        $clone.find('.degree').attr('id', 'degree'+ letter);
+        //update startdate id
+        $clone.find('.startDate').attr('id', 'startDate'+ letter);
+        //update enddate id
+        $clone.find('.endDate').attr('id', 'endDate'+ letter);
+        //update program id
+        $clone.find('.program').attr('id', 'program'+ letter);
 
         $clone.attr("data-index", index);
         $(".formContainer").append($clone);
@@ -21,6 +32,19 @@ $(document).ready(function() {
         const $clone = $(".experience").first().clone();
         let index = parseInt($(".experience").attr("data-index")) + experienceCounter;
         index = index.toString();
+        let letter = String.fromCharCode(65 + experienceCounter);
+        //update employer id
+        let newEmployer = 'employer' + letter;
+        $clone.find('.employer').attr('id', newEmployer);
+        // $clone.find('#employer').css("background-color", "red");
+        //update title id
+        $clone.find('.jobTitle').attr('id', 'title'+ letter);
+        //update start id
+        $clone.find('.jobStart').attr('id', 'jobStart'+ letter);
+        //update end id
+        $clone.find('.jobEnd').attr('id', 'jobEnd'+ letter);
+        //update description
+        $clone.find('.description').attr('id', 'description'+ letter);
 
         $clone.attr("data-index", index);
         $(".formContainer").append($clone);
@@ -33,6 +57,11 @@ $(document).ready(function() {
         
         let index = parseInt($(".project").attr("data-index")) + projectCounter;
         index = index.toString();
+        let letter = String.fromCharCode(65 + projectCounter);
+        //update project id
+        $clone.find('.project').attr('id', 'project'+ letter);
+        //update summary id
+        $clone.find('.summary').attr('id', 'summary'+ letter);
 
         $clone.attr("data-index", index);
         $(".formContainer").append($clone);
@@ -52,74 +81,175 @@ $(document).ready(function() {
         if (resumeName == "") {
             alert("Please enter a valid file name for your resume");
         };
-        // console.log("Resume Name: " + resumeName);
-        // console.log("Objective: " + objective);
-        // console.log("Skills: " + skills);
-        // console.log("Awards: " + awards);
         
     //define Resume object for resume post route call
      
         
-        let educationArray = [];
-        for (let i = 0; i <= educationCounter; i++) {
-            if (i == 0 && $(".school").eq(i).val() == "") {
-                break;
-            }
-            let education = {
-                school: $(".school").eq(i).val(),
-                degree: $(".degree").eq(i).val(),
-                start: $(".schoolStartDate").eq(i).val(),
-                end: $(".schoolEndDate").eq(i).val(),
-                program: $(".program").eq(i).val()
-            };
-            console.log(education);
-            educationArray.push(education);
-            //Call post route for EDUCATION OBJECT
-        }
-        let experienceArray = [];
-        for (let i = 0; i <= experienceCounter; i++) {
-            if (i == 0 && $(".employer").eq(i).val() == "") {
-                break;
-            }
-            let experience = {
-                employer: $(".employer").eq(i).val(),
-                title: $(".jobTitle").eq(i).val(),
-                start: $(".jobStartDate").eq(i).val(),
-                end: $(".jobEndDate").eq(i).val(),
-                description: $(".description").eq(i).val(),
-            };
-            console.log(experience);
-            experienceArray.push(experience);
-            //Call post route for EXPERIENCE OBJECT
-        }
-        let projectArray = [];
-        for (let i = 0; i <= projectCounter; i++) {
-            if (i == 0 && $(".projectTitle").eq(i).val() == "") {
-                break;
-            }
-            let project = {
-                title: $(".projectTitle").eq(i).val(),
-                description: $(".projectDescription").eq(i).val(),
-            };
-            console.log(project);
-            projectArray.push(project);
-            //Call post route for PROJECT OBJECT
-        }
+        // let educationArray = [];
+        // for (let i = 0; i <= educationCounter; i++) {
+        //     if (i == 0 && $(".school").eq(i).val() == "") {
+        //         break;
+        //     }
+        //     let education = {
+        //         school: $(".school").eq(i).val(),
+        //         degree: $(".degree").eq(i).val(),
+        //         start: $(".schoolStartDate").eq(i).val(),
+        //         end: $(".schoolEndDate").eq(i).val(),
+        //         program: $(".program").eq(i).val()
+        //     };
+        //     console.log(education);
+        //     educationArray.push(education);
+        //     //Call post route for EDUCATION OBJECT
+        // }
+        // let experienceArray = [];
+        // for (let i = 0; i <= experienceCounter; i++) {
+        //     if (i == 0 && $(".employer").eq(i).val() == "") {
+        //         break;
+        //     }
+        //     let experience = {
+        //         employer: $(".employer").eq(i).val(),
+        //         title: $(".jobTitle").eq(i).val(),
+        //         start: $(".jobStartDate").eq(i).val(),
+        //         end: $(".jobEndDate").eq(i).val(),
+        //         description: $(".description").eq(i).val(),
+        //     };
+        //     console.log(experience);
+        //     experienceArray.push(experience);
+        //     //Call post route for EXPERIENCE OBJECT
+        // }
+        // let projectArray = [];
+        // for (let i = 0; i <= projectCounter; i++) {
+        //     if (i == 0 && $(".projectTitle").eq(i).val() == "") {
+        //         break;
+        //     }
+        //     let project = {
+        //         title: $(".projectTitle").eq(i).val(),
+        //         description: $(".projectDescription").eq(i).val(),
+        //     };
+        //     console.log(project);
+        //     projectArray.push(project);
+        //     //Call post route for PROJECT OBJECT
+        // }
         
-        const resume = 
-        {
-            resumeName: resumeFileName,
-            objective: objectiveStatement,
-            //later include skills and awards
-            educations: educationArray,
-            experiences: experienceArray,
-            projects: projectArray,
-        };
+       const school = $(".school").val();
+       const degree = $(".degree").val();
+       const startDate = $(".startDate").val();
+       const endDate = $(".endDate").val();
+       const program = $(".program").val();
+       const isSchool = $(".school").val() === "" || $(".school").val() === undefined ? false: true;
+
+       const schoolB = $("#schoolB").val();
+       const degreeB = $("#degreeB").val();
+       const startDateB = $("#startDateB").val();
+       const endDateB = $("#endDateB").val();
+       const programB = $("#programB").val();
+       const isSchoolB = $("#schoolB").val() === "" || $("#schoolB").val() === undefined ? false: true;
+
+       const schoolC = $("#schoolC").val();
+       const degreeC = $("#degreeC").val();
+       const startDateC = $("#startDateC").val();
+       const endDateC = $("#endDateC").val();
+       const programC = $("#programC").val();
+       const isSchoolC = $("#schoolC").val() === "" || $("#schoolC").val() === undefined ? false: true;
+
+       const employer = $(".employer").val();
+       const title = $(".title").val();
+       const jobStart = $(".jobStart").val();
+       const jobEnd = $(".jobEnd").val();
+       const description = $(".description").val();
+       const isEmployer = $(".employer").val() === "" || $(".employer").val() === undefined ? false: true;
+
+       const employerB = $("#employerB").val();
+       const titleB = $("#titleB").val();
+       const jobStartB = $("#jobStartB").val();
+       const jobEndB = $("#jobEndB").val();
+       const descriptionB = $("#descriptionB").val();
+       const isEmployerB = $("#employerB").val() === "" || $("#employerB").val() === undefined ? false: true;
+
+       const employerC = $("#employerC").val();
+       const titleC = $("#titleC").val();
+       const jobStartC = $("#jobStartC").val();
+       const jobEndC = $("#jobEndC").val();
+       const descriptionC = $("#descriptionC").val();
+       const isEmployerC = $("#employerC").val() === "" || $("#employerC").val() === undefined ? false: true;
+
+       const project = $("#project").val();
+       const summary = $(".summary").val();
+       const isProject = $("#project").val() === "" || $("#project").val() === undefined ? false: true;
+
+       const projectB = $("#projectB").val();
+       const summaryB = $("#summaryB").val();
+       const isProjectB = $("#projectB").val() === "" || $("#projectB").val() === undefined? false: true;
+
+       const projectC = $("#projectC").val();
+       const summaryC = $("#summaryC").val();
+       const isProjectC = $("#projectC").val() === "" || $("#projectC").val() === undefined ? false: true;
+
+const userId = JSON.parse(localStorage.getItem("userId"));
+
+
+    const resume = {
+        userId: userId,
+        resumeName: resumeFileName,
+        objective: objectiveStatement,
+
+        school: school,
+        degree: degree,
+        startDate: startDate,
+        endDate: endDate,
+        program: program,
+        isSchool: isSchool,
+        schoolB: schoolB,
+        degreeB: degreeB,
+        startDateB: startDateB,
+        endDateB: endDateB,
+        programB: programB,
+        isSchoolB: isSchoolB,
+        schoolC: schoolC,
+        degreeC: degreeC,
+        startDateC: startDateC,
+        endDateC: endDateC,
+        programC: programC,
+        isSchoolC: isSchoolC,
         
+
+        employer: employer,
+        title: title,
+        jobStart: jobStart,
+        jobEnd: jobEnd,
+        description: description,
+        isEmployer: isEmployer,
+        employerB: employerB,
+        titleB: titleB,
+        jobStartB: jobStartB,
+        jobEndB: jobEndB,
+        descriptionB: descriptionB,
+        isEmployerB: isEmployerB,
+        employerC: employerC,
+        titleC: titleC,
+        jobStartC: jobStartC,
+        jobEndC: jobEndC,
+        descriptionC: descriptionC,
+        isEmployerC: isEmployerC,
+
+        project: project,
+        summary: summary,
+        isProject: isProject,
+        projectB: projectB,
+        summaryB: summaryB,
+        isProjectB: isProjectB,
+        projectC: projectC,
+        summaryC: summaryC,
+        isProjectC: isProjectC,
+
+        skills: skills,
+        awards: awards,
+    }
+        console.log(resume);
         $.ajax({
             method: "POST",
             url: "/api/resumes",
-            data: resume
+            data: resume,
         }).then(function(res) {
             console.log("posted resume");
             res.send(res);
--- seeds.js
+++ seeds.js
@@ -10,12 +10,74 @@ let seed = {
             phone: 99999999,
         })
     },
-    addResume: function(){
-        db.Resume.create({
+    // addResume: function(){
+    //     db.Resume.create({
 
-            resumeName: "test",
-            UserId: 1
-        })
+    //         resumeName: "test",
+    //         UserId: 1
+    //     })
+    // }
+    addResume: function() {
+        //this would come from the req.body in the server
+        // const { userId, name, resumeBody, etc } = resumeObj
+ 
+        db.Resume.create({
+            userId: 1,
+            resumeName: "secondName",
+            objective: "To see if this works",
+            school: "Georgia Tech",
+            degreeA: "Bachelor of Science",
+            startDate: "2012",
+            endDateA: "2016",
+            program: "Business",
+            isSchool: true,
+            employer: "Google",
+            title: "Software Engineer",
+            jobStart: "2018",
+            jobEnd: "2020",
+            description: "Did some stuff, not super exciting",
+            isEmployer: true,
+            schoolB: "Georgia Tech",
+            degreeB: "certificate",
+            startDateB: "2020",
+            endDateB: "2021",
+            programB: "Full Stack Web Develompent",
+            isSchoolB: true,
+            project: "Resume Builder",
+            summary: "Full Crud",
+            isProject: true,
+            // education: [
+            //     {
+            //         school: "Georgia Tech",
+            //         degree: "Bachelor of Science",
+            //         startDate: "2012",
+            //         endDate: "2018",
+            //         program: "Chemical Engineering"
+            //     },
+            //     {
+            //         school: "Georgia Tech",
+            //         degree: "Certificate",
+            //         startDate: "2020",
+            //         endDate: "2021",
+            //         program: "Full Stack Web Development"
+            //     }
+            // ],
+            // experience: [
+            //     {
+            //         employer: "GP",
+            //         title: "engineer",
+            //         startDate: "2018",
+            //         endDate: "2020",
+            //         description: "stuff"
+            //     }
+            // ],
+            // projects: [
+            //     {
+            //         title: "free from all",
+            //         description: "coding"
+            //     }
+            // ]
+        });
     }
     
 }
